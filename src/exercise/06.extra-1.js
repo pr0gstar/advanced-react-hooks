af20a4d4f21644218480bd5eebf4a6f94a18982c
Christoph Planken <c.planken@absturzsicherung.de>
2023-01-27 10:27:47 +0100

    added 06 extra 1

--- src/exercise/06.extra-1.js
+++ src/exercise/06.extra-1.js
@@ -3,9 +3,11 @@
 
 import * as React from 'react'
 
+const formatMediaDebugValue = ({query, state}) => `\`${query}\` => ${state}`
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
-  React.useDebugValue(`\`${query}\` => ${state}`)
+  React.useDebugValue({query, state}, formatMediaDebugValue)
 
   React.useEffect(() => {
     let mounted = true
